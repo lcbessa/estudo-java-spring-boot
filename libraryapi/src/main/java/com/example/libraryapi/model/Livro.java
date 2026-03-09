712c3e7ec48245de4e25cfb2afd97307fd51931f
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2026-03-09 11:34:30 -0300

    Adiciona annotation @Builder

--- libraryapi/src/main/java/com/example/libraryapi/model/Livro.java
+++ libraryapi/src/main/java/com/example/libraryapi/model/Livro.java
@@ -1,6 +1,7 @@
 package com.example.libraryapi.model;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +11,7 @@ import java.util.UUID;
 
 @Getter
 @Setter
+@Builder
 @Entity
 @Table(name = "livro")
 public class Livro {
