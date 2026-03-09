e7a332dbd64832bab4c690a7acb69b7060ca764e
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2026-03-09 13:56:00 -0300

    Adiciona repositories

--- libraryapi/src/main/java/com/example/libraryapi/model/GeneroLivro.java
+++ libraryapi/src/main/java/com/example/libraryapi/model/GeneroLivro.java
@@ -1,11 +1,5 @@
 package com.example.libraryapi.model;
 
 public enum GeneroLivro {
-    FICCAO,
-    FANTASIA,
-    MISTERIO,
-    ROMANCE,
-    BIOGRAFIA,
-    CIENCIA
-
+    FICCAO, FANTASIA, MISTERIO, ROMANCE, BIOGRAFIA, CIENCIA
 }
--- libraryapi/src/main/java/com/example/libraryapi/repository/LivroRepository.java
+++ libraryapi/src/main/java/com/example/libraryapi/repository/LivroRepository.java
@@ -1,4 +1,11 @@
 package com.example.libraryapi.repository;
 
-public interface LivroRepository {
+import com.example.libraryapi.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, UUID> {
 }
