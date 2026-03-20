88a63120c32d4be21f676aa0828ea45f0f6fb950
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2026-03-20 15:24:54 -0300

    Adiciona projeto de clientes

---
+++ clientespedidosprodutosapi/src/main/java/com/example/clientespedidosprodutosapi/ClientespedidosprodutosapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.clientespedidosprodutosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientespedidosprodutosapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientespedidosprodutosapiApplication.class, args);
+	}
+
+}
